82e657505262396649ad50ac803b9def845655e7
DavideBS <104096563+DavideBS@users.noreply.github.com>
2023-03-10 18:10:27 +0100

    fetch index.js

--- js/index.js
+++ js/index.js
@@ -1,25 +1,47 @@
 let squareLoop = 100;
 const rowSquare = document.getElementById("row-square");
+const colSquare = document.getElementById("col-square");
 
 
 
 
 
-for(let i = 2; i < squareLoop; i++){
-    rowSquare.innerHTML += `<div id="col-square" class="p-10 col">
-                                <div class="square bg-blue">
-                                    <p>${i}</p>
-                                </div>
-                            </div>`                     
-                           
-    if(i % 3 === 0){
+
+for(let i = 2; i < squareLoop +1; i++){
+                    
+
+   
+
+    if(i % 3 === 0 && i % 5 === 0){
+        rowSquare.innerHTML += `<div id="col-square" class="p-10 col">
+                            <div class="square bg-red">
+                                <p>${i} FIZZBUZZ</p>
+                            </div>
+                        </div>`
+
+    } else if(i % 3 === 0) {
         rowSquare.innerHTML += `<div id="col-square" class="p-10 col">
                                 <div class="square bg-green">
-                                    <p>FIZZ</p>
+                                    <p>${i} FIZZ</p>
                                 </div>
-                            </div>`
-       
+                            </div>`                  
+    } else if (i % 5 === 0){
+        rowSquare.innerHTML += `<div id="col-square" class="p-10 col">
+        <div class="square bg-yellow">
+            <p>${i} BUZZ</p>
+        </div>
+    </div>`
+    } else {
+        rowSquare.innerHTML += `<div id="col-square" class="p-10 col">
+        <div class="square bg-blue">
+            <p>${i}</p>
+        </div>
+    </div>`
     }
+
+
+
+    
 }
 
 
